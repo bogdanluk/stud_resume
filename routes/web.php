fb0552391c2c24d822458b57bfc875f50d04df24
bogdanluk <bog4594@gmail.com>
2023-01-31 12:25:49 +0300

    исправлены пути роутов авторизации

--- routes/web.php
+++ routes/web.php
@@ -24,11 +24,11 @@ Route::get('/', function () {
 })->name('home');
 
 Route::get('/login', function () {
-    return view('login');
+    return view('auth.login');
 })->name('login');
 
 Route::get('/register', function () {
-    return view('register');
+    return view('auth.register');
 })->name('register');
 
 //роуты для email
