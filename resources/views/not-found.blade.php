cbb6d47a893b15dc29bbdd067e263810a6857d4a
bogdanluk <bog4594@gmail.com>
2023-02-05 21:16:10 +0300

    Добавлен редирект на страницу 404, при переходе на несуществующий пост

--- resources/views/not-found.blade.php
+++ resources/views/not-found.blade.php
@@ -10,7 +10,7 @@
                 Извините!
             </p>
             <p class="mt-4 text-slate-400">Такой страницы не существует.</p>
-            <a href="{{ route('home') }}" class="inline-block px-5 py-3 mt-6 text-sm text-d font-medium text-black bg-yellow-400 rounded hover:bg-yellow-600 focus:outline-none focus:ring">
+            <a href="{{ route('home') }}" class="inline-block px-5 py-3 mt-6 text-sm text-d font-medium text-black bg-yellow-400 rounded hover:bg-yellow-600 outline-none focus:ring focus:ring-yellow-400">
                 На главную
             </a>
         </div>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.head')
 
-@section('title') {{$posts_data['name']}} @endsection
+@section('title') {{$post->name}} @endsection
 
 @section('content')
     @include('layouts.navbar')
 
     <div class="flex justify-center w-full h-96 mt-16">
-        <img src="/storage/news/{{ $posts_data->image  }}" alt="{{ $posts_data->image }}"/>
+        <img src="/storage/news/{{ $post->image }}" alt="{{ $post->image}}"/>
     </div>
 
     <div class="flex flex-col items-center w-full">
-        <h1 class="text-2xl text-center p-5 m-10 border-b-4 border-b-violet-400 rounded-lg">{{$posts_data['name']}}</h1>
+        <h1 class="text-2xl text-center p-5 m-10 border-b-4 border-b-violet-400 rounded-lg">{{$post->name}}</h1>
 
         <div class="flex flex-col w-11/12 bg-white rounded-lg shadow-lg">
             <p class="p-5">
-                {{$posts_data['content']}}
+                {{$post->content}}
             </p>
 
-            <p class="m-5 text-end text-gray-400">Дата создания: {{ $posts_data['created_at'] }}</p>
+            <p class="m-5 text-end text-gray-400">Дата создания: {{ $post->created_at}}</p>
         </div>
 
     </div>
