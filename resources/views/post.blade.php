ef26f30bb1010ea2e01dc6f842b67448f57befbe
bogdanluk <bog4594@gmail.com>
2023-02-06 14:05:18 +0300

    Изменены стили страниц с новостями, добавлена тёмная тема

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,26 +1,19 @@
 @extends('layouts.head')
 
-@section('title') {{$post->name}} @endsection
+@section('title'){{$post->name}}
+@endsection
 
 @section('content')
     @include('layouts.navbar')
-
-    <div class="flex justify-center w-full h-96 mt-16">
-        <img src="/storage/news/{{ $post->image }}" alt="{{ $post->image}}"/>
-    </div>
-
-    <div class="flex flex-col items-center w-full">
-        <h1 class="text-2xl text-center p-5 m-10 border-b-4 border-b-violet-400 rounded-lg">{{$post->name}}</h1>
-
-        <div class="flex flex-col w-11/12 bg-white rounded-lg shadow-lg">
-            <p class="p-5">
-                {{$post->content}}
-            </p>
-
-            <p class="m-5 text-end text-gray-400">Дата создания: {{ $post->created_at}}</p>
+    <div class="flex w-full min-h-screen justify-center">
+        <div class="flex flex-col mt-10 px-5 md:w-4/5">
+            <img class="flex max-h-96 mx-auto" src="/storage/news/{{ $post->image }}" alt="{{ $post->image}}"/>
+            <h1 class="text-2xl text-center p-5 text-violet-400">{{$post->name}}</h1>
+            <pre class="mb-2 font-sans font-normal">{{$post->content}}</pre>
+            <p class="w-full text-end text-slate-400">Дата создания: {{ $post->created_at}}</p>
         </div>
 
-    </div>
 
+    </div>
     @include('layouts.footer')
 @endsection
