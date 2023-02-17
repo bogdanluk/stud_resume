327886352254486998690b16b3042b1f1dc8feb4
bogdanluk <bog4594@gmail.com>
2023-02-17 11:47:40 +0300

    Добавлена иконка сайта

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <link rel="icon" type="image/jpg" href="{{ asset('img/site-icon.jpg') }}">
     <title>@yield('title')</title>
     <!-- Fonts -->
 
