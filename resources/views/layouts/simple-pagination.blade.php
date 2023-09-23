0bc0562c535caea918f0fac2616aee16f738a379
bogdanluk <bog4594@gmail.com>
2023-09-23 21:09:14 +0300

    Добавлено выравнивание для пагинации

--- resources/views/layouts/simple-pagination.blade.php
+++ resources/views/layouts/simple-pagination.blade.php
@@ -1,17 +1,19 @@
 @if ($paginator->hasPages())
-    <div class="btn-group items-center">
-        @if ($paginator->onFirstPage())
-            <span class="btn btn-outline btn-primary btn-disabled text-xl"><i class="fa-solid fa-angles-left"></i></span>
-        @else
-            <a href="{{ $paginator->previousPageUrl() }}" class="btn btn-outline btn-primary text-xl"><i class="fa-solid fa-angles-left"></i></a>
-        @endif
+    <div class="flex w-full justify-center">
+        <div class="btn-group items-center">
+            @if ($paginator->onFirstPage())
+                <span class="btn btn-outline btn-primary btn-disabled text-xl"><i class="fa-solid fa-angles-left"></i></span>
+            @else
+                <a href="{{ $paginator->previousPageUrl() }}" class="btn btn-outline btn-primary text-xl"><i class="fa-solid fa-angles-left"></i></a>
+            @endif
 
-        <span class="btn btn-outline btn-primary font-semibold text-xl">{{ $paginator->currentPage() }}</span>
+            <span class="btn btn-outline btn-primary font-semibold text-xl">{{ $paginator->currentPage() }}</span>
 
-        @if ($paginator->hasMorePages())
-            <a href="{{ $paginator->nextPageUrl() }}" class="btn btn-outline btn-primary text-xl"><i class="fa-solid fa-angles-right"></i></a>
-        @else
-            <span class="btn btn-outline btn-primary btn-disabled text-xl"><i class="fa-solid fa-angles-right"></i></span>
-        @endif
+            @if ($paginator->hasMorePages())
+                <a href="{{ $paginator->nextPageUrl() }}" class="btn btn-outline btn-primary text-xl"><i class="fa-solid fa-angles-right"></i></a>
+            @else
+                <span class="btn btn-outline btn-primary btn-disabled text-xl"><i class="fa-solid fa-angles-right"></i></span>
+            @endif
+        </div>
     </div>
 @endif
